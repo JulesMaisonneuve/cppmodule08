07293c9b8fb41ba2300a9dfb070d6918f17541f6
Jules Maisonneuve <jumaison@student.42.fr>
2022-05-11 14:36:03 +0200

    need tests

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -30,6 +30,19 @@ class MutantStack : public std::stack<T>
 
 		};
 
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+		iterator begin() {return std::stack<T>::c.begin();};
+		iterator end() {return std::stack<T>::c.end();};
+		const_iterator cbegin() const {return std::stack<T>::c.cbegin();};
+		const_iterator cend() const {return std::stack<T>::c.cend();};
+		reverse_iterator rbegin() {return std::stack<T>::c.rbegin();};
+		reverse_iterator rend() {return std::stack<T>::c.rend();};
+		const_reverse_iterator crbegin() const {return std::stack<T>::c.crbegin();};
+		const_reverse_iterator crend() const {return std::stack<T>::c.crend();};
 };
 
 #endif
